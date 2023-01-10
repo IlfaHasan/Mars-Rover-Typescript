479bd3e20cd37e7e7998db2beaccf5b40ea91dca
ilfa <ilfahassan@gmail.com>
2023-01-10 22:21:14 +0000

    Refactored turnRight functionality

--- mars-Rover-kata/src/rover.test.ts
+++ mars-Rover-kata/src/rover.test.ts
@@ -1,3 +1,7 @@
+type Heading = "N" | "W" | "S" | "E";
+const direction: Array<Heading> = ["N", "E", "S", "W"];
+
+
 const turnLeft = (x: string) => {
   switch (x) {
     case "N":
@@ -11,43 +15,35 @@ const turnLeft = (x: string) => {
   }
 };
 
-const turnRight = (x: string) => {
-    switch (x) {
-      case "N":
-        return "E";
-      case "E":
-        return "S";
-      case "S":
-        return "W";
-      case "W":
-        return "N";
-    }
-  };
-
-//turnLeft
+//turnRight
+const turnRight = (heading: Heading) => {
+    const index = direction.indexOf(heading);
+    return direction[(index+1)%4];
+}
+ //turnLeft
 test("When facing N,turn left should face the Rover W", () => {
-    expect(turnLeft("N")).toBe("W");
-  });
-  test("When facing W,turn left should face the Rover S", () => {
-    expect(turnLeft("W")).toBe("S");
-  });
-  test("When facing S,turn left should face the Rover E", () => {
-    expect(turnLeft("S")).toBe("E");
-  });
-  test("When facing E,turn left should face the Rover N", () => {
-    expect(turnLeft("E")).toBe("N");
-  });
+  expect(turnLeft("N")).toBe("W");
+});
+test("When facing W,turn left should face the Rover S", () => {
+  expect(turnLeft("W")).toBe("S");
+});
+test("When facing S,turn left should face the Rover E", () => {
+  expect(turnLeft("S")).toBe("E");
+});
+test("When facing E,turn left should face the Rover N", () => {
+  expect(turnLeft("E")).toBe("N");
+});
 
 //turnRight
-  test("When facing N,turn left should face the Rover W", () => {
-    expect(turnRight("N")).toBe("E");
-  });
-  test("When facing W,turn left should face the Rover S", () => {
-    expect(turnRight("E")).toBe("S");
-  });
-  test("When facing S,turn left should face the Rover E", () => {
-    expect(turnRight("S")).toBe("W");
-  });
-  test("When facing E,turn left should face the Rover N", () => {
-    expect(turnRight("W")).toBe("N");
-  });
+test("When facing N,turn right should face the Rover W", () => {
+  expect(turnRight("N")).toBe("E");
+});
+test("When facing W,turn right should face the Rover S", () => {
+  expect(turnRight("E")).toBe("S");
+});
+test("When facing S,turn right should face the Rover E", () => {
+  expect(turnRight("S")).toBe("W");
+});
+test("When facing E,turn right should face the Rover N", () => {
+  expect(turnRight("W")).toBe("N");
+});

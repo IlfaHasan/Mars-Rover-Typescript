d1b8844b02ffdbdbecabd21e6875bedca2c66ff2
ilfa <ilfahassan@gmail.com>
2023-01-10 20:44:20 +0000

    Implemented turnLeft function of the rover

--- mars-Rover-kata/src/rover.test.ts
+++ mars-Rover-kata/src/rover.test.ts
@@ -1,5 +1,19 @@
-const turnLeft=(x:string)=> "W";
+const turnLeft=(x:string)=>{
+    if(x==="N") return "W";
+    if(x==="W") return "S";
+    if(x==="S") return "E";
+    return "N"
+}
 
 test("When facing N,turn left should face the Rover W",()=>{
     expect(turnLeft("N")).toBe("W");
+})
+test("When facing W,turn left should face the Rover S",()=>{
+    expect(turnLeft("W")).toBe("S");
+})
+test("When facing S,turn left should face the Rover E",()=>{
+    expect(turnLeft("S")).toBe("E");
+})
+test("When facing E,turn left should face the Rover N",()=>{
+    expect(turnLeft("E")).toBe("N");
 })

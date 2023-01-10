d28378608d1f7d36b25b1cce89ad1e2fdcece119
ilfa <ilfahassan@gmail.com>
2023-01-10 20:49:22 +0000

    Replaced repeated if statements with switch for turnLeft fuction

--- mars-Rover-kata/src/rover.test.ts
+++ mars-Rover-kata/src/rover.test.ts
@@ -1,19 +1,25 @@
-const turnLeft=(x:string)=>{
-    if(x==="N") return "W";
-    if(x==="W") return "S";
-    if(x==="S") return "E";
-    return "N"
-}
+const turnLeft = (x: string) => {
+  switch (x) {
+    case "N":
+      return "W";
+    case "W":
+      return "S";
+    case "S":
+      return "E";
+    case "E":
+      return "N";
+  }
+};
 
-test("When facing N,turn left should face the Rover W",()=>{
-    expect(turnLeft("N")).toBe("W");
-})
-test("When facing W,turn left should face the Rover S",()=>{
-    expect(turnLeft("W")).toBe("S");
-})
-test("When facing S,turn left should face the Rover E",()=>{
-    expect(turnLeft("S")).toBe("E");
-})
-test("When facing E,turn left should face the Rover N",()=>{
-    expect(turnLeft("E")).toBe("N");
-})
+test("When facing N,turn left should face the Rover W", () => {
+  expect(turnLeft("N")).toBe("W");
+});
+test("When facing W,turn left should face the Rover S", () => {
+  expect(turnLeft("W")).toBe("S");
+});
+test("When facing S,turn left should face the Rover E", () => {
+  expect(turnLeft("S")).toBe("E");
+});
+test("When facing E,turn left should face the Rover N", () => {
+  expect(turnLeft("E")).toBe("N");
+});

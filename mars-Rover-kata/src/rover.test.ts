8976446fb1a61f4615cd2a2bed254203df4cc937
ilfa <ilfahassan@gmail.com>
2023-01-10 23:23:17 +0000

    Implemented move function of the rover.So basic functionalities of the rover done

--- mars-Rover-kata/src/rover.test.ts
+++ mars-Rover-kata/src/rover.test.ts
@@ -1,18 +1,27 @@
 type Heading = "N" | "W" | "S" | "E";
+type Coordinates = [x: number, y: number];
 const direction: Array<Heading> = ["N", "E", "S", "W"];
 
+const moveRover = (heading: Heading, position: Coordinates) => {
+  const [x, y] = position;
+  if (heading === "N") return [x, y + 1];
+  if (heading === "E") return [x + 1, y];
+  if (heading === "S") return [x, y - 1];
+  return [x - 1, y];
+};
 
-const turnLeft =(heading: Heading) => {
-    const index = direction.indexOf(heading);
-    return direction[(index+3)%4];
-}
+//turnLeft
+const turnLeft = (heading: Heading) => {
+  const index = direction.indexOf(heading);
+  return direction[(index + 3) % 4];
+};
 
 //turnRight
 const turnRight = (heading: Heading) => {
-    const index = direction.indexOf(heading);
-    return direction[(index+1)%4];
-}
- //turnLeft
+  const index = direction.indexOf(heading);
+  return direction[(index + 1) % 4];
+};
+
 test("When facing N,turn left should face the Rover W", () => {
   expect(turnLeft("N")).toBe("W");
 });
@@ -39,3 +48,16 @@ test("When facing S,turn right should face the Rover E", () => {
 test("When facing E,turn right should face the Rover N", () => {
   expect(turnRight("W")).toBe("N");
 });
+
+test("When moving N,increment y coordinate by 1 keeping x coordinate unchanged", () => {
+  expect(moveRover("N", [1, 1])).toEqual([1, 2]);
+});
+test("When moving E,increment x coordinate by 1 keeping y coordinate unchanged", () => {
+  expect(moveRover("E", [1, 1])).toEqual([2, 1]);
+});
+test("When moving S,decrement y coordinate by 1 keeping x coordinate unchanged", () => {
+    expect(moveRover("S", [1, 1])).toEqual([1, 0]);
+  });
+  test("When moving W,decrement x coordinate by 1 keeping y coordinate unchanged", () => {
+    expect(moveRover("W", [1, 1])).toEqual([0, 1]);
+  });
